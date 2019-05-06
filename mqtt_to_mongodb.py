a92a79183d2a869a00b8ab38fc6b2665ff0c9946
mario.pereira <mario.pereira>
2019-05-07 00:24:12 +0200

    removed mongofunction

--- mqtt_to_mongodb.py
+++ mqtt_to_mongodb.py
@@ -8,17 +8,11 @@ from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure, InvalidName
 import __future__
 
-
-def externalconnections():
-    mongodb_conn = os.environ['mongodb_connnection']
-    mqtt_server_conn = os.environ['mqtt_conn']
-    return mongodb_conn, mqtt_server_conn
-
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("opcua/#")
+    client.subscribe("/opcua/#")
 
-def on_message(client, userdata, msg,collection):
+def on_message(client, userdata, msg, collection):
     receiveTime = datetime.datetime.now()
     message = msg.payload.decode("utf-8")
     isfloatValue = False
@@ -36,20 +30,18 @@ def on_message(client, userdata, msg,collection):
         print(str(receiveTime) + ": " + msg.topic + " " + message)
         post = {"time": receiveTime, "topic": msg.topic, "value": message}
 
-    collection.insert_one(post)
-
-def mongodb_connection(mongodb_conn):
+    mongodb_conn = os.environ['mongodb_connnection']
     mongoClient = MongoClient(mongodb_conn)
     db = mongoClient.plc_poc_db
     collection = db.plc_poc
-    return collection
+    collection.insert_one(post)
 
-def mqtt_connection(collection, mqtt_server_conn):
+def topic_to_mongo():
+    mqtt_server_conn = os.environ['mqtt_conn']
     client = mqtt.Client(mqtt_server_conn)
     client.on_connect = on_connect
     client.on_message = on_message
     client.loop_forever()
 
-
 def function_handler(event, context):
-    return
+    topic_to_mongo()
