21f838b5b411ce3627c05b4df7b76a7d39fe21df
mario.pereira <mario.pereira>
2019-05-06 19:31:04 +0200

    changed code

--- mqtt_to_mongodb.py
+++ mqtt_to_mongodb.py
@@ -7,47 +7,53 @@ import os
 from pymongo import MongoClient
 import __future__
 
-def mqtt_topic():
-    mqtt_server_conn = os.environ['mqtt_conn']
-    client = mqtt.Client()
-    client.on_connect = mqtt_topic
-    client.on_message = write_to_mongo
-    connOK = False
-    while(connOK == False):
-        try:
-            client.connect(mqtt_server_conn, 1883, 60)
-            connOK = True
-        except:
-            connOK = False
-        time.sleep(2)
-    # Blocking loop to the Mosquitto broker
-    client.loop_forever()
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
     client.subscribe("opcua/#")
 
-def write_to_mongo(client, userdata, msg):
-    mongodb_conn = os.environ['mongodb_connnection']
-    mongoClient = MongoClient(mongodb_conn)
-    db = mongoClient.plc_poc_db
-    collection = db.plc_poc
-    receivetime = datetime.datetime.now()
+
+def on_message(client, userdata, msg):
+    receiveTime = datetime.datetime.now()
     message = msg.payload.decode("utf-8")
-    isvaluefloat = False
+    isfloatValue = False
     try:
         # Convert the string to a float so that it is stored as a number and not a string in the database
         val = float(message)
-        isvaluefloat = True
+        isfloatValue = True
     except:
-        isvaluefloat = False
+        isfloatValue = False
 
-    if isvaluefloat:
-        print(str(receivetime) + ": " + msg.topic + " " + str(val))
-        post = {"time": receivetime, "topic": msg.topic, "value": val}
+    if isfloatValue:
+        print(str(receiveTime) + ": " + msg.topic + " " + str(val))
+        post = {"time": receiveTime, "topic": msg.topic, "value": val}
     else:
-        print(str(receivetime) + ": " + msg.topic + " " + message)
-        post = {"time": receivetime, "topic": msg.topic, "value": message}
+        print(str(receiveTime) + ": " + msg.topic + " " + message)
+        post = {"time": receiveTime, "topic": msg.topic, "value": message}
 
-    collection.insert_one(post)
 
+# Set up client for MongoDB
+mongodb_conn = os.environ['mongodb_connnection']
+mongoClient = MongoClient(mongodb_conn)
+db = mongoClient.plc_poc_db
+collection = db.plc_poc
+
+# Initialize the client that should connect to the Mosquitto broker
+mqtt_server_conn = os.environ['mqtt_conn']
+client = mqtt.Client(mqtt_server_conn)
+client.on_connect = on_connect
+client.on_message = on_message
+connOK = False
+while(connOK == False):
+    try:
+        client.connect("mqtt_server_conn", 1883, 60)
+        connOK = True
+    except:
+        connOK = False
+    time.sleep(2)
+
+# Blocking loop to the Mosquitto broker
+client.loop_forever()
 
 def function_handler(event, context):
     return
